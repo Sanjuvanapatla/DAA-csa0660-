8420aab07ed0ffe33a69bb39957af406ce9b8993
Sanjuvanapatla <100762152+Sanjuvanapatla@users.noreply.github.com>
2022-10-06 13:51:26 +0530

    Add files via upload

---
+++ optical_binary.c
@@ -0,0 +1,19 @@
+Implementation of Kruskal's Algorithm
+ 
+Enter the no. of vertices:3  
+ 
+Enter the cost adjacency matrix: 
+9
+8
+7
+6
+5
+4
+3
+2
+3
+The edges of Minimum Cost Spanning Tree are  
+1 edge (3,2) =2  
+2 edge (3,1) =3  
+ 
+Minimum cost = 5
